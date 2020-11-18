4101bec3383fe2007658b298a7cf0188582d98bc
bohdan <protas>
2020-11-18 10:38:28 +0200

    Project setup

---
+++ RxMovies/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  RxMovies
+//
+//  Created by protas on 18.11.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
