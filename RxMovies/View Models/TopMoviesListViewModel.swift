69c7e1e2f5163d5ffa182a1efa5bcfd4a3f1a3fd
bohdan <protas>
2020-11-18 11:25:46 +0200

    Setup view model and binding

--- RxMovies/View Models/TopMoviesListViewModel.swift
+++ RxMovies/View Models/TopMoviesListViewModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import RxSwift
+
+class TopMoviesListViewModel {
+    
+    /// Errors that describe state of view model
+    public enum MoviesListViewModelError: Error {
+        case unknown
+        case textDescribed(descr: String)
+    }
+    
+    // MARK: Properties
+    
+    private let bag = DisposeBag()
+    
+    // MARK: Output
+    
+    private(set) var topRatedMovies: PublishSubject<[TopRatedMovieModel]> = PublishSubject()
+    private(set) var error: PublishSubject<MoviesListViewModelError> = PublishSubject()
+    
+    // MARK: Public
+    
+    func fetchDataSource() {
+        MoviesAPIService.shared.topRated().subscribe {
+            switch $0 {
+            case .next(let movies):
+                let mappedModels = movies.map {
+                    TopRatedMovieModel(
+                        id: $0.id,
+                        title: $0.title,
+                        overview: $0.overview,
+                        posterURL: MoviesAPIService.imageBase.appendingPathComponent($0.poster_path ?? "")
+                    )
+                }
+                DispatchQueue.main.async {
+                    self.topRatedMovies.onNext(mappedModels)
+                }
+            case .error(let error):
+                self.error.onNext(.textDescribed(descr: error.localizedDescription))
+            case .completed:
+                print("done")
+            }
+        }.disposed(by: bag)
+    }
+    
+}
--- RxMovies/Views/TopMoviesListViewController.swift
+++ RxMovies/Views/TopMoviesListViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Kingfisher
 
 class TopMoviesListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    private let bag = DisposeBag()
+    private let viewModel = TopMoviesListViewModel()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
+        bindUI()
+        viewModel.fetchDataSource()
     }
 
+    // MARK: - Privates
+    
+    private func setupUI() {
+        tableView.estimatedRowHeight = 230
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    private func bindUI() {
+
+        // binding data source
+        viewModel.topRatedMovies.bind(to: tableView.rx.items(cellIdentifier: "TopMovieTableViewCell", cellType: TopMovieTableViewCell.self)) { row, model, cell in
+            cell.titleLabel.text = model.title
+            cell.bodyLabel.text = model.overview
+            cell.posterImageView?.kf.setImage(with: model.posterURL)
+        }.disposed(by: bag)
+        
+        // observing errors to show
+        viewModel.error
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { (error) in
+                switch error {
+                case .unknown:
+                    print("An uknown error occured")
+                case .textDescribed(let descr):
+                    print("An error occured: \(descr)")
+                }
+            })
+            .disposed(by: bag)
+    }
+    
+    
 }
